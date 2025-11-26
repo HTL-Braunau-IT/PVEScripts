68e0105ae70712398481d46df3db3766765b07e3
Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
2025-11-26 08:29:49 +0100

    Update windows11-vm.sh

--- vm/windows11-vm.sh
+++ vm/windows11-vm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#Test
+
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: michelroegl-brunner
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
